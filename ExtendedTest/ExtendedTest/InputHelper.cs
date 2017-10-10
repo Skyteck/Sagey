50d015cc7ecc2822e12e7ca6a75dd33685095a53
SkyArmada <sky.armada@gmail.com>
2017-10-10 17:14:01 -0500

    Starting InputHelper

---
+++ ExtendedTest/ExtendedTest/InputHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendedTest
+{
+    public static class InputHelper
+    {
+        static MouseState PrevMouseState;
+        static MouseState CurrentMouseState;
+        static KeyboardState PrevKBState;
+        static KeyboardState CurrentKBState;
+
+        static InputHelper()
+        {
+            PrevKBState = new KeyboardState();
+            CurrentKBState = Keyboard.GetState();
+            PrevMouseState = new MouseState();
+            CurrentMouseState = Mouse.GetState();
+        }
+
+        public static void Update()
+        {
+            PrevKBState = CurrentKBState;
+            PrevMouseState = CurrentMouseState;
+
+            CurrentKBState = Keyboard.GetState();
+            CurrentMouseState = Mouse.GetState();
+        }
+
+
+    }
+}
